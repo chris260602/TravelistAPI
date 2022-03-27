98b104ad79f00e557a39390fa790bd83b2840dc6
chris260602 <christoperlim20@gmail.com>
2022-03-27 16:50:28 +0700

    Fixing Bug

--- errorHandling.js
+++ errorHandling.js
@@ -2,7 +2,7 @@ exports.catchAsync = (fn, msg) => (req, res, next) => {
   fn(req, res, next).catch((e) => {
     console.log(e);
     res.status(400).json({
-      error: msg,
+      error: e,
     });
   });
 };
