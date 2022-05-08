29bdcf204e21194451cc53e34a56287725f49729
chris260602 <christoperlim20@gmail.com>
2022-05-08 18:22:24 +0700

    Email

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -24,7 +24,7 @@ const createTransporter = async () => {
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
-    port: 465,
+    port: 80,
     secure: true,
     auth: {
       type: "OAuth2",
